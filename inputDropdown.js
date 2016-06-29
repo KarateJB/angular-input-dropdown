0f4aa9232463d0e53c683038985df179e9585aab
KarateJB <ppippenkimo@gamil.com>
2016-06-29 10:22:30 +0800

    Fix a default-dropdown-items binding bug.

--- inputDropdown.js
+++ inputDropdown.js
@@ -1,204 +1,220 @@
 angular.module('inputDropdown', []).directive('inputDropdown', [function () {
-  var templateString =
-      '<div class="input-dropdown">' +
-      '<input type="text"' +
-      'name="{{inputName}}"' +
-      'placeholder="{{inputPlaceholder}}"' +
-      'ng-model="inputValue"' +
-      'value="{{inputValue}}"' +
-      'ng-required="inputRequired"' +
-      'ng-change="inputChange()"' +
-      'ng-focus="inputFocus()"' +
-      'ng-blur="inputBlur($event)"' +
-      'input-dropdown-validator>' +
-      '<ul ng-show="dropdownVisible">' +
-      '<li ng-repeat="item in dropdownItems"' +
-      'ng-click="selectItem(item)"' +
-      'ng-mouseenter="setActive($index)"' +
-      'ng-mousedown="dropdownPressed()"' +
-      'ng-class="{\'active\': activeItemIndex === $index}"' +
-      '>' +
-      '<span ng-if="item.readableName">{{item.readableName}}</span>' +
-      '<span ng-if="!item.readableName">{{item}}</span>' +
-      '</li>' +
-      '</ul>' +
-      '</div>';
+    var templateString =
+        '<div class="input-dropdown">' +
+        '<input type="text"' +
+        'name="{{inputName}}"' +
+        'placeholder="{{inputPlaceholder}}"' +
+        'ng-model="inputValue"' +
+        'value="{{inputValue}}"' +
+        'ng-required="inputRequired"' +
+        'ng-change="inputChange()"' +
+        'ng-focus="inputFocus()"' +
+        'ng-blur="inputBlur($event)"' +
+        'input-dropdown-validator>' +
+        '<ul ng-show="dropdownVisible">' +
+        '<li ng-repeat="item in dropdownItems"' +
+        'ng-click="selectItem(item)"' +
+        'ng-mouseenter="setActive($index)"' +
+        'ng-mousedown="dropdownPressed()"' +
+        'ng-class="{\'active\': activeItemIndex === $index}"' +
+        '>' +
+        '<span ng-if="item.readableName">{{item.readableName}}</span>' +
+        '<span ng-if="!item.readableName">{{item}}</span>' +
+        '</li>' +
+        '</ul>' +
+        '</div>';
 
-  return {
-    restrict: 'E',
-    scope: {
-      inputValue: "=ngModel",
-      defaultDropdownItems: '=',
-      selectedItem: '=',
-      inputRequired: '=',
-      inputName: '@',
-      inputPlaceholder: '@',
-      filterListMethod: '&',
-      itemSelectedMethod: '&'
-    },
-    template: templateString,
-    controller: function ($scope) {
-      this.getSelectedItem = function () {
-        return $scope.selectedItem;
-      };
-      this.isRequired = function () {
-        return $scope.inputRequired;
-      };
-    },
-    link: function (scope, element) {
-      var pressedDropdown = false;
-      var inputScope = element.find('input').isolateScope();
+    return {
+        restrict: 'E',
+        scope: {
+            inputValue: "=ngModel",
+            defaultDropdownItems: '=',
+            selectedItem: '=',
+            inputRequired: '=',
+            inputName: '@',
+            inputPlaceholder: '@',
+            filterListMethod: '&',
+            itemSelectedMethod: '&',
+            focusIn: '&',
+            focusOut: '&'
+        },
+        template: templateString,
+        controller: function ($scope) {
+            this.getSelectedItem = function () {
+                return $scope.selectedItem;
+            };
+            this.isRequired = function () {
+                return $scope.inputRequired;
+            };
+        },
+        link: function (scope, element) {
+            var pressedDropdown = false;
+            var inputScope = element.find('input').isolateScope();
 
-      scope.activeItemIndex = 0;
-      scope.inputValue = '';
-      scope.dropdownVisible = false;
-      scope.dropdownItems = scope.defaultDropdownItems || [];
+            scope.activeItemIndex = 0;
+            scope.inputValue = '';
+            scope.dropdownVisible = false;
+            scope.dropdownItems = scope.defaultDropdownItems || [];
 
-      scope.$watch('dropdownItems', function (newValue, oldValue) {
-        if (!angular.equals(newValue, oldValue)) {
-          // If new dropdownItems were retrieved, reset active item
-          scope.setActive(0);
-        }
-      });
-
-      scope.$watch('selectedItem', function (newValue, oldValue) {
-        inputScope.updateInputValidity();
-
-        if (!angular.equals(newValue, oldValue)) {
-          if (newValue) {
-            // Update value in input field to match readableName of selected item
-            if (typeof newValue === 'string') {
-              scope.inputValue = newValue;
-            }
-            else {
-              scope.inputValue = newValue.readableName;
-            }
-          }
-          else {
-            // Uncomment to clear input field when editing it after making a selection
-            // scope.inputValue = '';
-          }
-        }
-      });
-
-      scope.setActive = function (itemIndex) {
-        scope.activeItemIndex = itemIndex;
-      };
-
-      scope.inputChange = function () {
-        scope.selectedItem = null;
-        showDropdown();
-
-        if (!scope.inputValue) {
-          scope.dropdownItems = scope.defaultDropdownItems || [];
-          return;
-        }
-
-        if (scope.filterListMethod) {
-          var promise = scope.filterListMethod({ userInput: scope.inputValue });
-          if (promise) {
-            promise.then(function (dropdownItems) {
-              scope.dropdownItems = dropdownItems;
+            scope.$watch('dropdownItems', function (newValue, oldValue) {
+                if (!angular.equals(newValue, oldValue)) {
+                    // If new dropdownItems were retrieved, reset active item
+                    scope.setActive(0);
+                }
             });
-          }
-        }
-      };
 
-      scope.inputFocus = function () {
-        scope.setActive(0);
-        showDropdown();
-      };
+            scope.$watch('defaultDropdownItems', function (newValue, oldValue) {
+                scope.dropdownItems = scope.defaultDropdownItems || [];
+            });
 
-      scope.inputBlur = function (event) {
-        if (pressedDropdown) {
-          // Blur event is triggered before click event, which means a click on a dropdown item wont be triggered if we hide the dropdown list here.
-          pressedDropdown = false;
-          return;
-        }
-        hideDropdown();
-      };
+            scope.$watch('selectedItem', function (newValue, oldValue) {
+                inputScope.updateInputValidity();
 
-      scope.dropdownPressed = function () {
-        pressedDropdown = true;
-      }
+                if (!angular.equals(newValue, oldValue)) {
+                    if (newValue) {
+                        // Update value in input field to match readableName of selected item
+                        if (typeof newValue === 'string') {
+                            scope.inputValue = newValue;
+                        }
+                        else {
+                            scope.inputValue = newValue.readableName;
+                        }
+                    }
+                    else {
+                        // Uncomment to clear input field when editing it after making a selection
+                        // scope.inputValue = '';
+                    }
+                }
+            });
 
-      scope.selectItem = function (item) {
-        scope.selectedItem = item;
-        hideDropdown();
-        scope.dropdownItems = [item];
+            scope.setActive = function (itemIndex) {
+                scope.activeItemIndex = itemIndex;
+            };
 
-        if (scope.itemSelectedMethod) {
-          scope.itemSelectedMethod({ item: item });
-        }
-      };
+            scope.inputChange = function () {
+                scope.selectedItem = null;
+                showDropdown();
 
-      var showDropdown = function () {
-        scope.dropdownVisible = true;
-      };
-      var hideDropdown = function () {
-        scope.dropdownVisible = false;
-      }
+                if (!scope.inputValue) {
+                    scope.dropdownItems = scope.defaultDropdownItems || [];
+                    return;
+                }
 
-      var selectPreviousItem = function () {
-        var prevIndex = scope.activeItemIndex - 1;
-        if (prevIndex >= 0) {
-          scope.setActive(prevIndex);
-        }
-      };
+                if (scope.filterListMethod) {
+                    var promise = scope.filterListMethod({ userInput: scope.inputValue });
+                    if (promise) {
+                        promise.then(function (dropdownItems) {
+                            scope.dropdownItems = dropdownItems;
+                        });
+                    }
+                }
+            };
 
-      var selectNextItem = function () {
-        var nextIndex = scope.activeItemIndex + 1;
-        if (nextIndex < scope.dropdownItems.length) {
-          scope.setActive(nextIndex);
-        }
-      };
+            scope.inputFocus = function () {
 
-      var selectActiveItem = function () {
-        if (scope.activeItemIndex >= 0 && scope.activeItemIndex < scope.dropdownItems.length) {
-          scope.selectItem(scope.dropdownItems[scope.activeItemIndex]);
-        }
-      };
+                if (scope.focusIn) {
+                    scope.focusIn();
+                }
 
-      element.bind("keydown keypress", function (event) {
-        switch (event.which) {
-          case 38: //up
-            scope.$apply(selectPreviousItem);
-            break;
-          case 40: //down
-            scope.$apply(selectNextItem);
-            break;
-          case 13: // return
-            if (scope.dropdownVisible && scope.dropdownItems && scope.dropdownItems.length > 0) {
-              // only preventDefault when there is a list so that we can submit form with return key after a selection is made
-              event.preventDefault();
-              scope.$apply(selectActiveItem);
+                scope.setActive(0);
+                showDropdown();
+            };
+
+            scope.inputBlur = function (event) {
+
+                if (scope.focusOut) {
+                    scope.focusOut();
+                }
+
+                if (pressedDropdown) {
+                    // Blur event is triggered before click event, which means a click on a dropdown item wont be triggered if we hide the dropdown list here.
+                    pressedDropdown = false;
+                    return;
+                }
+                hideDropdown();
+            };
+
+            scope.dropdownPressed = function () {
+                pressedDropdown = true;
             }
-            break;
+
+            scope.selectItem = function (item) {
+                scope.selectedItem = item;
+                hideDropdown();
+                scope.dropdownItems = [item];
+
+                if (scope.itemSelectedMethod) {
+                    scope.itemSelectedMethod({ item: item });
+                }
+            };
+
+            var showDropdown = function () {
+                scope.dropdownVisible = true;
+            };
+            var hideDropdown = function () {
+                scope.dropdownVisible = false;
+            }
+
+            var selectPreviousItem = function () {
+                var prevIndex = scope.activeItemIndex - 1;
+                if (prevIndex >= 0) {
+                    scope.setActive(prevIndex);
+                }
+            };
+
+            var selectNextItem = function () {
+                var nextIndex = scope.activeItemIndex + 1;
+                if (nextIndex < scope.dropdownItems.length) {
+                    scope.setActive(nextIndex);
+                }
+            };
+
+            var selectActiveItem = function () {
+                if (scope.activeItemIndex >= 0 && scope.activeItemIndex < scope.dropdownItems.length) {
+                    scope.selectItem(scope.dropdownItems[scope.activeItemIndex]);
+                }
+            };
+
+            element.bind("keydown keypress", function (event) {
+                switch (event.which) {
+                    case 38: //up
+                        scope.$apply(selectPreviousItem);
+                        break;
+                    case 40: //down
+                        scope.$apply(selectNextItem);
+                        break;
+                    case 13: // return
+                        if (scope.dropdownVisible && scope.dropdownItems && scope.dropdownItems.length > 0) {
+                            // only preventDefault when there is a list so that we can submit form with return key after a selection is made
+                            event.preventDefault();
+                            scope.$apply(selectActiveItem);
+                        }
+                        break;
+                }
+            });
         }
-      });
     }
-  }
 }]);
 
 angular.module('inputDropdown').directive('inputDropdownValidator', function () {
-  return {
-    require: ['^inputDropdown', 'ngModel'],
-    restrict: 'A',
-    scope: {},
-    link: function (scope, element, attrs, ctrls) {
-      var inputDropdownCtrl = ctrls[0];
-      var ngModelCtrl = ctrls[1];
-      var validatorName = 'itemSelectedValid';
+    return {
+        require: ['^inputDropdown', 'ngModel'],
+        restrict: 'A',
+        scope: {},
+        link: function (scope, element, attrs, ctrls) {
+            var inputDropdownCtrl = ctrls[0];
+            var ngModelCtrl = ctrls[1];
+            var validatorName = 'itemSelectedValid';
 
-      scope.updateInputValidity = function () {
-        var selection = inputDropdownCtrl.getSelectedItem();
-        if (selection || !inputDropdownCtrl.isRequired()) {
-          ngModelCtrl.$setValidity(validatorName, true);
+            scope.updateInputValidity = function () {
+                var selection = inputDropdownCtrl.getSelectedItem();
+                if (selection || !inputDropdownCtrl.isRequired()) {
+                    ngModelCtrl.$setValidity(validatorName, true);
+                }
+                else {
+                    ngModelCtrl.$setValidity(validatorName, false);
+                }
+            };
         }
-        else {
-          ngModelCtrl.$setValidity(validatorName, false);
-        }
-      };
-    }
-  };
+    };
 });
